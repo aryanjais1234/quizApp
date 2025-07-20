e9548cac18d48d56f91b12e523d5d16846e33782
aryanjais1234 <aryanjais1234@gmail.com>
2025-07-20 14:28:07 +0530

    Added quizQuestionResponse and created one one controller for same

--- backend/microservices/quizApp/src/main/java/com/Quiz/quizApp/controller/QuizController.java
+++ backend/microservices/quizApp/src/main/java/com/Quiz/quizApp/controller/QuizController.java
@@ -1,13 +1,14 @@
 package com.Quiz.quizApp.controller;
 
+import com.Quiz.quizApp.dto.QuizQuestionResponse;
+import com.Quiz.quizApp.model.Question;
 import com.Quiz.quizApp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -19,6 +20,11 @@ public class QuizController {
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam Integer numQ, @RequestParam String title){
         return new ResponseEntity<>(quizService.createQuiz(category,numQ,title), HttpStatus.OK);
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuizQuestionResponse>> getQuizQuestions(@PathVariable Integer id){
+        return new ResponseEntity<>(quizService.getQuizQuestions(id),HttpStatus.OK);
     }
 
     
--- backend/microservices/quizApp/src/main/java/com/Quiz/quizApp/service/QuizService.java
+++ backend/microservices/quizApp/src/main/java/com/Quiz/quizApp/service/QuizService.java
@@ -2,12 +2,15 @@ package com.Quiz.quizApp.service;
 
 import com.Quiz.quizApp.dao.QuestionDao;
 import com.Quiz.quizApp.dao.QuizDao;
+import com.Quiz.quizApp.dto.QuizQuestionResponse;
 import com.Quiz.quizApp.model.Question;
 import com.Quiz.quizApp.model.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class QuizService {
@@ -29,5 +32,24 @@ public class QuizService {
         quizDao.save(quiz);
 
         return "Success";
+    }
+
+    public List<QuizQuestionResponse> getQuizQuestions(Integer id) {
+        Optional<Quiz> quiz = quizDao.findById(id);
+
+        return quiz.get().getQuestions().stream()
+                .map(this::mapToQuestionResponse)
+                .collect(Collectors.toList());
+    }
+
+    private QuizQuestionResponse mapToQuestionResponse(Question question){
+        return new QuizQuestionResponse(
+                question.getId(),
+                question.getQuestionTitle(),
+                question.getOption1(),
+                question.getOption2(),
+                question.getOption3(),
+                question.getOption4()
+        );
     }
 }
